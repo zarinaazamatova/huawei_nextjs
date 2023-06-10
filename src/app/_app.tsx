e5a4d17eece7690c6e22ff2fcfba036c485ad792
Zhyldyz <jargynbaeva@gmail.com>
2023-06-10 23:07:04 +0200

    not done yet

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -1,6 +1,7 @@
-import './globals.css'
-import type { AppProps } from 'next/app'
+import React from 'react';
+import './globals.css';
+import type { AppProps } from 'next/app';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 
@@ -10,14 +11,15 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to
+          <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
+          Get started by editing
+          <code>pages/index.js</code>
         </p>
 
         <div className={styles.grid}>
@@ -49,7 +51,8 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by
+          {' '}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
