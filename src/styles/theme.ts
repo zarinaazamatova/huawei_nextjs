a96fb7a676ae559a158d2cfeef11b0343d5a4364
Zarina Azamatova <93516049+zarinaazamatova@users.noreply.github.com>
2023-06-24 11:21:54 +0200

    fix

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,11 +1,9 @@
 const defaultTheme = {
-  // screens for a vast array of web-capable  browsers.
   screens: {
     s: '480px',
     m: '768px',
     l: '1024px',
   },
-  // spacing
   spacing: {
     '1': '8px',
     '2': '12px',
@@ -14,12 +12,10 @@ const defaultTheme = {
     '5': '32px',
     '6': '48px',
   },
-  // font family
   fontFamily: {
     roboto: ['Roboto', 'sans-serif'],
     arial: ['Arial', 'sans-serif'],
   },
-  // font sizes
   fontSizes: {
     sm: '12px',
     base: '14px',
@@ -28,13 +24,11 @@ const defaultTheme = {
     xl: '24px',
     xxl: '30px',
   },
-  // font weights
   fontWeights: {
     light: 300,
     normal: 400,
     bold: 700,
   },
-  // background color
   colors: {
     primary: '#1d364b',
     secondary: '#8da2b5',
@@ -44,7 +38,6 @@ const defaultTheme = {
     white: '#ffffff',
     darkgGrey: 'rgba(29,54,75,.5)',
   },
-  // text color
   textColors: {
     black: '#000000',
     white: '#ffffff',
