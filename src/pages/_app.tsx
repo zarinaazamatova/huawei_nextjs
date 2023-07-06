bb4270a92f5a52f925584f237d89e65ab530b648
Alexander Zharkov <aleksandr.zharkov1@epam.com>
2023-07-07 01:49:26 +0600

    [HNJ-10] Fix broken _app.tsx file error from eslint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,12 @@ import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 import defaultTheme from '@/styles/theme';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ThemeProvider theme={defaultTheme}>
       <Component {...pageProps} />
     </ThemeProvider>
   );
-}
+};
+
+export default MyApp;
