daccc5e8feaeee708ebb02f93ed2048b87770d16
Alexander Zharkov <aleksandr.zharkov1@epam.com>
2023-07-21 01:07:20 +0600

    Fix 6

--- src/components/SimpleSlider/SimpleSlider.tsx
+++ src/components/SimpleSlider/SimpleSlider.tsx
@@ -4,49 +4,40 @@ import 'slick-carousel/slick/slick-theme.css';
 import Image from 'next/image';
 import { StyledSlider } from './SimpleSlider.styles';
 import { useSliderConfigs } from './Slider.hooks';
-import {
-  grandin,
-  hiPet,
-  royalCanin,
-  unica,
-  ownat,
-  hills,
-  wellKiss,
-} from '../../../public/assets/images';
 
 const sliderImages = [
   {
-    image: grandin,
+    image: '/assets/images/grandin.webp',
     alt: 'Главная: Grandin: до -30% на сухой и влажный корм для кошек и собак',
     id: 0,
   },
   {
-    image: hills,
+    image: '/assets/images/hills.webp',
     alt: 'Главная: Hill`s: до -50% на сухой и влажный корм для кошек и собак',
     id: 1,
   },
   {
-    image: hiPet,
+    image: '/assets/images/hiPet.webp',
     alt: 'Главная: Новая коллекция HiPet',
     id: 2,
   },
   {
-    image: ownat,
+    image: '/assets/images/ownat.webp',
     alt: 'Главная: Ownat',
     id: 3,
   },
   {
-    image: royalCanin,
+    image: '/assets/images/royalCanin.jpg',
     alt: 'Главная: Royal Canin: -15% на сухой корм для кошек и собак',
     id: 4,
   },
   {
-    image: unica,
+    image: '/assets/images/unica.webp',
     alt: 'Главная: Unica: 1+1 сухой корм для кошек в подарок',
     id: 5,
   },
   {
-    image: wellKiss,
+    image: '/assets/images/wellKiss.webp',
     alt: 'Главная: Скидки до 50% на корма, товары ветаптеки и аксессуары для питомцев ',
     id: 6,
   },
@@ -59,7 +50,7 @@ export const SimpleSlider = () => {
       {sliderImages.map((sliderImage) => {
         return (
           <div key={sliderImage.id}>
-            <Image src={sliderImage.image} alt={sliderImage.alt} />
+            <Image src={sliderImage.image} alt={sliderImage.alt} width={1300} height={280} />
           </div>
         );
       })}
