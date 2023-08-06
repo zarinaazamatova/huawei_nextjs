4bc6292867b8fe49f47fbed54eb3a1c7f6760663
Zarina Azamatova <93516049+zarinaazamatova@users.noreply.github.com>
2023-08-06 22:20:19 +0200

    delete unnecessary file

--- src/components/Arrow/Arrow.styles.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-import { ArrowIcon } from '../../../public/assets/svgs/ArrowIcon';
-
-export const StyledArrowIcon = styled(ArrowIcon)`
-  transform: 180deg;
-  color: blue;
-`;
