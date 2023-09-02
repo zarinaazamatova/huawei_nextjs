5c94656397ebb2cdff69ba7589c4e0e4f898196d
Zhyldyz <jargynbaeva@gmail.com>
2023-09-02 12:56:38 +0200

    'added map to some of the singleNews'

--- src/components/SingleNews/SingleNews.tsx
+++ src/components/SingleNews/SingleNews.tsx
@@ -6,14 +6,27 @@ import {
   StyledParagraph,
   StyledImage,
 } from './SingleNews.style';
+import { GoogleMapComponent } from '../GoogleMap';
 
-export const SingleNews = ({ id, title, date, category, desc, img }: News) => {
+export const SingleNews = ({
+  id,
+  title,
+  date,
+  category,
+  address,
+  desc,
+  img,
+  latitude,
+  longitude,
+}: News) => {
   return (
     <StyledBox key={id}>
       <StyledHeading>{title}</StyledHeading>
       <StyledDate>{date}</StyledDate>
       <StyledImage src={img} alt={category} width={0} height={0} sizes="100vw" />
       <StyledParagraph>{desc}</StyledParagraph>
+      <StyledParagraph>{address}</StyledParagraph>
+      {latitude && longitude && <GoogleMapComponent latitude={latitude} longitude={longitude} />}
     </StyledBox>
   );
 };
